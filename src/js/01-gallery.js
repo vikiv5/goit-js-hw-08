734d7f6f551e45420b4891054bb5a4288c9f013d
vikiv5 <vi_kov@ukr.net>
2023-01-19 02:34:29 +0200

    hw 1-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+const imgList = document.querySelector ('.gallery'); 
+const imgMarkup = createImagesMarkup (galleryItems);
+imgList.insertAdjacentHTML('beforeend', imgMarkup);
+
+ function createImagesMarkup (galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+       return ` <li><a class = "gallery__link " href="${original}">
+       <img 
+       class = "gallery__image" 
+       src = "${preview}"
+       alt = "${description}"/>
+       </a></li>`;
+    }) .join ("");
+ 
+}
+
+new SimpleLightbox('.gallery a', 
+{
+captions:true,
+captionsData:"alt" ,
+captionDelay : 250 ,
+ }) ;
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = ({seconds}) => {
+    localStorage.setItem("videoplayer-current-time", seconds);
+    // data is an object containing properties specific to that event
+};
+
+player.on("timeupdate", throttle(onPlay,1000));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
