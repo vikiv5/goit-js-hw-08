c42337f8ac7b52df1c840f77592e9797ca6d986d
vikiv5 <vi_kov@ukr.net>
2023-01-20 02:11:26 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
+
+const feedbackForm = document.querySelector ("feedback-form");
+const emailInput = document.querySelector ("input");
+const messageInput = document.querySelector ("textarea");
+
+
+feedbackForm.addEventListener("submit", onSubmitForm)
+
+function onSubmitForm (evt){
+evt.preventDefault();
+evt.currentTarget.reset();
+localStorage.removeItem("feedback-form-state")
+}
+
+feedbackForm.addEventListener("input", throttle(onTextareaInput ,500))
+function onTextareaInput(evt) {
+const message = evt.target.value ;
+
+
+localStorage.setItem ("feedback-form-state" , message)
+}
+
+function returnLocalStorageData (){
+const savedMessage = localStorage.getItem("feedback-form-state");
+const parseMessage = JSON.parse(savedMessage);
+
+if(savedMessage) {
+    console.log(parseMessage)
+    messageInput.value=parseMessage.email||"" ;
+    emailInput.value=parseMessage.message|| "" ;
+}
+};
 
